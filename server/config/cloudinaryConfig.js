ff094ac0656821f6b55decf2274a0480b0ee038e
DHRUV PRUTHI <68409876+dhruvpruthi29@users.noreply.github.com>
2023-09-15 12:37:36 +0530

    init

---
+++ server/config/cloudinaryConfig.js
@@ -0,0 +1,12 @@
+
+
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({
+    cloud_name: process.env.cloud_name,
+    api_key: process.env.cloud_api_key,
+    api_secret: process.env.cloud_api_secret,
+    secure: true,
+});
+
+module.exports = cloudinary;
